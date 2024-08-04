7ace0b17f6d3bb72af7561f2fde703d8d481b39d
regent-ndongmo <jauressndongmo5287@gmail.com>
2024-08-04 18:17:04 +0100

    Deploy Angular app to GitHub Pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2L5SEA54.mjs";import"./chunk-3FHZL27G.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-QGPNYLW6.mjs";import"./chunk-JIGNILYT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
